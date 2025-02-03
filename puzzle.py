fb947fc3db2c224d0aa301e78a50a7a481c39670
Roman Pavelka <roman.pavelka.asi@gmail.com>
2025-02-04 00:42:29 +0100

    Solved

--- puzzle.py
+++ puzzle.py
@@ -6,30 +6,43 @@ def puzzle_solver(pieces, width, height):
                 return convert_to_solution(placed)
         placed, remaining = attempts.pop()
         attempts.extend(find_all_possible_next_steps(placed, remaining, width, height))
-        print(len(attempts))
+        # print(len(attempts))
 
 
 def find_all_possible_next_steps(placed, remaining, width, height):
-    print(convert_to_solution(placed))
-    # Handle first top left piece
-    if not placed:
-        i, top_left = [(i, piece) for (i, piece) in enumerate(remaining)
-                       if piece[0][0] is None and piece[0][1] is None and piece[1][0] is None][0]
-        placed = [[top_left], ]
-        return [(placed, remaining[:i] + remaining[i:])]
+    # print(convert_to_solution(placed))
+    
     last_placed_row = len(placed) - 1
-    last_placed_column = len(placed[-1]) - 1
-    # Handle middle of first row
-    if last_placed_row == 0 and last_placed_column < width - 1:
-        candidates = [(i, piece) for (i, piece) in enumerate(remaining)
-                      if piece[0][0] is None and piece[0][1] is None
-                      and piece[1][0] == placed[0][last_placed_column][1][1] and piece[1][0] is not None]
-        return [([placed[0] + [c]], [remaining[:i] + remaining[i:]]) for (i, c) in candidates]
+    if placed:
+        last_placed_column = len(placed[-1]) - 1
 
-    return []
+    def matches(piece, placed):
+        # Top left
+        if not placed:
+            return (piece[0][0], piece[0][1], piece[1][0]) == (None, None, None)
+        piece_left = placed[-1][last_placed_column]
+        # First row
+        if len(placed) == 1 and len(placed[0]) < width:
+            return (piece[0][0], piece[0][1], piece[1][0]) == (None, None, piece_left[1][1])
+        # First column
+        if len(placed[-1]) == width:
+            piece_up = placed[-1][0]
+            return (piece[0][0], piece[0][1], piece[1][0]) == (None, piece_up[1][1], None)
+        # Rest
+        piece_up = placed[-2][last_placed_column + 1]
+        return (piece[0][0], piece[0][1], piece[0][0], piece[1][0]) == (piece_up[1][0], piece_up[1][1], piece_left[0][1], piece_left[1][1])
+    
+    candidates = [(i, piece) for (i, piece) in enumerate(remaining) if matches(piece, placed)]
+
+    if placed and last_placed_column < width - 1:
+        return [(placed[:last_placed_row] + [placed[last_placed_row] + [c]],
+                 remaining[:i] + remaining[i+1:]) for (i, c) in candidates]
+    
+    return [(placed + [[c,]], remaining[:i] + remaining[i+1:]) for (i, c) in candidates]
 
 
 def convert_to_solution(placed):
+    #print(placed)
     return [[i for (first_row, second_row, i) in row] for row in placed]
 
 
